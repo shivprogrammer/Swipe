cf0d3cd20a5c122c5fa79bc7eea1d0d416bebf08
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-04 20:59:23 -0800

    card stacking styling effect working [iOS WORKING, Android strange card rendering bug]

--- shivvyswipe/src/Deck.js
+++ shivvyswipe/src/Deck.js
@@ -88,15 +88,22 @@ class Deck extends Component {
         return (
           <Animated.View
             key={item.id}
-            style={this.getCardStyle()}
+            style={[this.getCardStyle(), styles.cardStyle]}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
           </Animated.View>
         );
       }
-      return this.props.renderCard(item);
-    });
+      return (
+        <Animated.View
+          key={item.id}
+          style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}
+        >
+          {this.props.renderCard(item)}
+        </Animated.View>
+      );
+    }).reverse();
   }
 
   render() {
@@ -107,5 +114,12 @@ class Deck extends Component {
     );
   }
 }
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
+};
 
 export default Deck;
